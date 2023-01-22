04cdb27e8a44357b6716617ef5cda6dd746b89e8
Fdasert <72107725+Fdasert@users.noreply.github.com>
2023-01-23 00:45:27 +0300

    Add files via upload

--- index.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {
-    var scroll = window.scrollY;
-    if (scroll > 50) {
-      document.body.style.backgroundColor = "red";
-    } else {
-      document.body.style.backgroundColor = "white";
-    }
-  };
-  
